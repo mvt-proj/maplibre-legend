a9a04c451263c809e18a30c18f45b91e5ecd1431
Jose Jachuf <jjachuf@gmail.com>
2025-05-05 21:55:03 -0300

    simplification of the example

--- example/src/main.rs
+++ example/src/main.rs
@@ -6,27 +6,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let legend = MapLibreLegend::new(&style_json, 250,40, true, true)?;
 
-    if let Some(svg) = legend.render_layer("ejes_calles", Some(true)) {
-        fs::write("ejes_calles.svg", svg)?;
-    }
-
-    if let Some(svg) = legend.render_layer("parcelario-fill", Some(true)) {
-        fs::write("parcelario_fill.svg", svg)?;
-    }
-
-    if let Some(svg) = legend.render_layer("osm-base", Some(true)) {
-        fs::write("osm.svg", svg)?;
-    }
-
-    if let Some(svg) = legend.render_layer("puntoscc", Some(true)) {
-        fs::write("puntoscc.svg", svg)?;
-    }
+    /// Render a single layer
     if let Some(svg) = legend.render_layer("vs2023", Some(true)) {
         fs::write("vs2023.svg", svg)?;
     }
 
-
-
+    /// Render all layers
     let combined = legend.render_all();
     fs::write("combined.svg", combined)?;
 
