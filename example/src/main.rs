471dfb9a50d2472cfa419e1c82e2a15d2da8bfe4
Jose Jachuf <jjachuf@gmail.com>
2025-05-08 20:00:01 -0300

    custom-labels

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,13 +2,13 @@ use maplibre_legend::MapLibreLegend;
 use std::fs;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let style_json = fs::read_to_string("style.json")?;
+    let style_json = fs::read_to_string("style3.json")?;
 
     let legend = MapLibreLegend::new(&style_json, 250,40, true, true)?;
 
-    if let Some(svg) = legend.render_layer("vs2023", Some(true)) {
-        fs::write("vs2023.svg", svg)?;
-    }
+    // if let Some(svg) = legend.render_layer("vs2023", Some(true)) {
+    //     fs::write("vs2023.svg", svg)?;
+    // }
 
     let combined = legend.render_all(false);
     fs::write("combined.svg", combined)?;
